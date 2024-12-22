110589a839d7fcdb072ae2371698e329f635cb4e
Dmytryienko_Tetiana <dmitrienko_tanya@ukr.net>
2024-12-22 19:31:02 +0200

    homework9 form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,12 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+const inputForm = document.querySelector('input');
+inputForm.classList.add('input-form-email');
+const textareaForm = document.querySelector('textarea');
+textareaForm.classList.add('textarea-form-message');
+const buttonForm = document.querySelector('button');
+buttonForm.classList.add('btn-form');
+
+const formData = { email: '', message: '' };
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', function (event) {
+  const form = event.target;
+});
