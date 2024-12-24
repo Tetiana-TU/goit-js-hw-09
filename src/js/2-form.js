99a5b055b3fa4841d269fdfc3fbe9e6e3cf36d9e
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2024-12-24 15:04:10 +0200

    Homework9 add  Form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,8 +5,58 @@ textareaForm.classList.add('textarea-form-message');
 const buttonForm = document.querySelector('button');
 buttonForm.classList.add('btn-form');
 
-const formData = { email: '', message: '' };
-const form = document.querySelector('.feedback-form');
-form.addEventListener('input', function (event) {
-  const form = event.target;
-});
+
+
+const feedbackFormEl = document.querySelector('.feedback-form');
+let formData = {
+   email: '',
+  message: '',
+};
+
+const fillFormFields = () => {
+  try {
+    if (localStorage.length === 0) {
+      return 'Fill please all fields';
+    }
+
+    const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    formData = formDataFromLS;
+
+    for (const key in formDataFromLS) {
+      feedbackFormEl.elements[key].value = formDataFromLS[key];
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fillFormFields();
+
+const onFormFieldInput = event => {
+  const { target: formFieldEl } = event;
+
+  const fieldValue = formFieldEl.value;
+  const fieldName = formFieldEl.name;
+
+  formData[fieldName] = fieldValue;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+const onFeedbackFormSubmit = event => {
+  event.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+  } 
+
+  const { currentTarget: formEl } = event;
+
+  formEl.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+feedbackFormEl.addEventListener('input', onFormFieldInput);
+feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
+  
+  
