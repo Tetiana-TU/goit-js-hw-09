d7ec9635d794831c4c6585d1f6ef9e7684bcbccb
Tetiana-TU <dmitrienko_tanya@ukr.net>
2024-12-24 13:06:27 +0000

    Deploying to gh-pages from @ Tetiana-TU/goit-js-hw-09@99a5b055b3fa4841d269fdfc3fbe9e6e3cf36d9e 🚀

--- assets/2-form-Cjh7RbM1.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("input");e.classList.add("input-form-email");const o=document.querySelector("textarea");o.classList.add("textarea-form-message");const r=document.querySelector("button");r.classList.add("btn-form");const n=document.querySelector(".feedback-form");n.addEventListener("input",function(t){t.target});
-//# sourceMappingURL=2-form-Cjh7RbM1.js.map
