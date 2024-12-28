84668ec792fbe31c1816145b417860e52328c243
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2024-12-28 18:33:25 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -48,9 +48,7 @@ const btnFormSubmit = event => {
    
   formEl.reset();
   localStorage.removeItem('feedback-form-state');
-formData.email = '';
-  formData.message = '';
-};
+  
 feedbackFormEl.addEventListener('input', funformInput);
 feedbackFormEl.addEventListener('submit', btnFormSubmit);
 
